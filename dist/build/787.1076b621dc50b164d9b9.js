cae83db18caa49005dd5efd2ca8fc8ec0ced3fa9
Alex Bogdanovich <7894664@mail.com>
2024-01-05 20:46:42 +0100

    delete extra folder

--- dist/build/787.1076b621dc50b164d9b9.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-(self.webpackChunkwebpack_ulbi = self.webpackChunkwebpack_ulbi || []).push([[787], { 787: function (e, c, u) { u.r(c), u.d(c, { default: function () { return s; } }); var n = u(893); var s = function () { return (0, n.jsx)("div", { children: (0, n.jsx)("h1", { children: "Shop" }) }); }; } }]);
--- dist/config/build/buildResolvers.js
+++
@@ -1,9 +0,0 @@
-export function buildResolvers(_a) {
-    var paths = _a.paths;
-    return {
-        extensions: [".tsx", ".ts", ".js"],
-        alias: {
-            "@": paths.src,
-        },
-    };
-}
